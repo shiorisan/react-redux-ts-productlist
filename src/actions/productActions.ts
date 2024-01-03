5daa9230c5c7f853e2981b62f2d267921d08f7bb
shiosaan <shiosaan@gmail.com>
2024-01-03 18:48:08 +0900

    add first sample mui to product list

--- src/actions/productActions.ts
+++ src/actions/productActions.ts
@@ -1,15 +1,25 @@
 import { Action } from 'redux';
 import { ThunkAction } from 'redux-thunk';
 import { RootState } from '../redux/store';
-import { Product, GET_PRODUCTS } from '../redux/reducers/productReducer';
+import { GET_PRODUCTS } from '../redux/reducers/productReducer';
 
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
+// interface ProductListProps {
+//   products: Product[];
+// }
 // Define async actions
 export const getProducts = (): ThunkAction<void, RootState, unknown, Action<string>> => async dispatch => {
   // Here you would usually make an API call to get the products, but for simplicity we will just return a static list
   const products: Product[] = [
-    { id: 1, name: 'Product 1', price: 100 },
-    { id: 2, name: 'Product 2', price: 200 },
-    { id: 3, name: 'Product 3', price: 300 },
+    { id: 1, name: 'Product 1', price: 100},
+    { id: 2, name: 'Product 2', price: 200},
+    { id: 3, name: 'Product 3', price: 300},
   ];
 
   dispatch({
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
-import { Product } from '../redux/reducers/productReducer';
+import { List, ListItem, ListItemText, ListItemAvatar, Avatar } from '@material-ui/core';
+import { Image as ImageIcon } from '@material-ui/icons';
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
 
 interface ProductListProps {
   products: Product[];
 }
 
-const ProductList: React.FC<ProductListProps> = ({ products }) => {
-  return (
-    <div>
-      {products.map((product) => (
-        <div key={product.id}>
-          <h2>{product.name}</h2>
-          <p>{product.price}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
+const ProductList: React.FC<ProductListProps> = ({ products }) => (
+  <List>
+    {products.map((product) => (
+      <ListItem key={product.id}>
+        <ListItemAvatar>
+          <Avatar>
+            <ImageIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary={product.name} secondary={product.price} />
+      </ListItem>
+    ))}
+  </List>
+);
 
 export default ProductList;
